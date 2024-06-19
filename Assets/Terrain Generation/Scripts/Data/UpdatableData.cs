ed6d3d0937a244aa5edc60860a74f2deb293ca97
Matthew Zeman <mzeman4818@baysidehighschool.org>
2024-06-18 22:46:00 -0400

    Add files via upload

---
+++ Assets/Terrain Generation/Scripts/Data/UpdatableData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class UpdatableData : ScriptableObject {
+
+	public event System.Action OnValuesUpdated;
+	public bool autoUpdate;
+
+	protected virtual void OnValidate() {
+		if (autoUpdate) {
+			#if UNITY_EDITOR
+						EditorApplication.update += NotifyOfUpdatedValues;
+			#endif
+		}
+	}
+
+	public void NotifyOfUpdatedValues() {
+		#if UNITY_EDITOR
+				EditorApplication.update -= NotifyOfUpdatedValues;
+		#endif
+		if (OnValuesUpdated != null) {
+			OnValuesUpdated ();
+		}
+	}
+
+}
