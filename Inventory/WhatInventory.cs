74a3a0c8670293a520dc9d67751c370336b99da3
Matthew Zeman <mzeman4818@baysidehighschool.org>
2024-06-18 22:48:49 -0400

    Delete Inventory directory

--- Inventory/WhatInventory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WhatInventory : MonoBehaviour
-{
-    public GameObject inventoryOpen1;
-    public GameObject inventoryOpen2;
-}
