84e4653cfb31df5f7942a6abeb574413f944aaf4
Matthew Zeman <mzeman4818@baysidehighschool.org>
2024-06-18 22:49:38 -0400

    Delete Terrain directory

--- Terrain/IsABreakable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IsABreakable : MonoBehaviour
-{
-    //NEED THIS EMPTY SCRIPT FOR CHECKING CROSSHAIR PLACEMENT DONT DELETE
-}
--- Terrain/IsACollectable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IsACollectable : MonoBehaviour
-{
-    //NEED THIS EMPTY SCRIPT FOR CHECKING CROSSHAIR PLACEMENT DONT DELETE
-}
